7526677a551ffefdecf99082463b20c546225747
BttrDrgn <49764143+BttrDrgn@users.noreply.github.com>
2022-03-24 21:26:57 -0500

    Set resolution

--- src/game/main.cpp
+++ src/game/main.cpp
@@ -3,11 +3,14 @@
 #include "patches/patches.hpp"
 
 #include <utils/exception/exception.hpp>
+#include <utils/hook/hook.hpp>
 
 int __cdecl main(int argc, char* argv[])
 {
 	utils::exception::init("client");
 	load("psobb.exe");
+	utils::hook::return_value(0x004829E0, 1280);
+	utils::hook::return_value(0x004829E8, 720);
 	patches::init();
 	return entry_point(0x00B60000);
 }
