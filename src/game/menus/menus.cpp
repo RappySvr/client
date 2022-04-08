29762ca270350b09a09b70251b942e056881c55a
BttrDrgn <49764143+BttrDrgn@users.noreply.github.com>
2022-04-08 14:16:35 -0500

    Text to screen hook

--- src/game/menus/menus.cpp
+++ src/game/menus/menus.cpp
@@ -11,7 +11,7 @@
 void* title_return = (void*)(0x006F4DAD);
 
 static utils::function<void __cdecl(float, float, float, int, wchar_t*)> text_to_screen_;
-void __cdecl text_to_screen(float pos_x, float pos_y, float a3, int a4, wchar_t* text)
+void __cdecl menus::text_to_screen(float pos_x, float pos_y, float a3, int a4, wchar_t* text)
 {
 	if (_ReturnAddress() == title_return)
 	{
@@ -47,7 +47,7 @@ void menus::news()
 		news::states::once = true;
 	}
 
-	text_to_screen_(menus::news_pos_x, 10.0f, 0.0f, -1, utils::format::c_to_w(&news::news_text[0]));
+	menus::text_to_screen(menus::news_pos_x, 10.0f, 0.0f, -1, utils::format::c_to_w(&news::news_text[0]));
 	menus::news_pos_x--;
 
 	if (menus::news_pos_x <= -menus::text_size(utils::format::c_to_w(&news::news_text[0])))
@@ -58,7 +58,7 @@ void menus::news()
 
 void menus::init()
 {
-	text_to_screen_ = utils::hook::detour(0x00789248, text_to_screen, 8);
+	text_to_screen_ = utils::hook::detour(0x00789248, menus::text_to_screen, 8);
 }
 
 float menus::news_pos_x = 0;
--- src/game/menus/menus.hpp
+++ src/game/menus/menus.hpp
@@ -6,6 +6,8 @@ public:
 	static void init();
 	static void news();
 
+	static void text_to_screen(float pos_x, float pos_y, float a3, int a4, wchar_t* text);
+
 	static float news_pos_x;
 	static float news_reset_pos_x;
 
