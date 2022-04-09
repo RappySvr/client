ae094470b0dd3a308d23b3823efc42dc461e434d
BttrDrgn <49764143+BttrDrgn@users.noreply.github.com>
2022-04-09 03:14:08 -0500

    Winapi hooks

--- src/game/winapi/winapi.cpp
+++ src/game/winapi/winapi.cpp
@@ -10,15 +10,24 @@ HWND __stdcall create_window_ex_a(DWORD dwExStyle, LPCSTR lpClassName, LPCSTR lp
 	int X, int Y, int nWidth, int nHeight, HWND hWndParent, HMENU hMenu, HINSTANCE hInstance, LPVOID lpParam)
 {
 	//Allow minimize and close
-	dwStyle = WS_SYSMENU | WS_MINIMIZEBOX;
+	if (*(int*)(0x00A46C72) == 1)
+	{
+		dwStyle = WS_SYSMENU | WS_MINIMIZEBOX;
+	}
 
 	winapi::hwnd = CreateWindowExA(dwExStyle, lpClassName, lpWindowName, dwStyle, X, Y, nWidth, nHeight, hWndParent, hMenu, hInstance, lpParam);
+
 #ifndef DISABLE_SDL
 	window::sdl_window = SDL_CreateWindowFrom(winapi::hwnd);
-	//SDL_SetWindowGrab(window::sdl_window, SDL_TRUE);
+	//window::sdl_renderer = SDL_CreateRenderer(window::sdl_window, 0, 0);
 #endif
 
 	return winapi::hwnd;
+}
+
+LONG __stdcall set_window_long_a(HWND hWnd, int nIndex, LONG dwNewLong)
+{
+	return SetWindowLongA(hWnd, nIndex, dwNewLong);
 }
 
 HANDLE __stdcall create_file_a(LPCSTR lpFileName, DWORD dwDesiredAccess, DWORD dwShareMode, LPSECURITY_ATTRIBUTES lpSecurityAttributes,
@@ -57,12 +66,31 @@ BOOL __stdcall create_directory_a(LPCSTR lpPathName, LPSECURITY_ATTRIBUTES lpSec
 
 int __stdcall show_cursor(bool show)
 {
-	return show;
+	return ShowCursor(show);
 }
 
 void __fastcall output_debug_string(LPCSTR ecx, char* edx)
 {
 	PRINT_DEBUG_N("%s", ecx);
+}
+
+void __stdcall sleep(DWORD dwMilliseconds)
+{
+#ifndef DISABLE_SDL
+	SDL_Delay(dwMilliseconds);
+#else
+	Sleep(dwMilliseconds);
+#endif
+}
+
+LRESULT __stdcall def_wndproc_a(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam)
+{
+	return DefWindowProcA(hWnd, Msg, wParam, lParam);
+}
+
+BOOL __stdcall set_cwd(LPCSTR lpPathName)
+{
+	return SetCurrentDirectoryA(lpPathName);
 }
 
 void winapi::init()
@@ -73,6 +101,10 @@ void winapi::init()
 	utils::hook::set(0x008F8334, show_cursor);
 	utils::hook::set(0x008F8144, create_file_a);
 	utils::hook::set(0x008F8220, create_directory_a);
+	utils::hook::set(0x008F83A0, set_window_long_a);
+	utils::hook::set(0x008F8174, sleep);
+	utils::hook::set(0x008F836C, def_wndproc_a);
+	utils::hook::set(0x008F8148, set_cwd);
 
 	utils::hook::detour(0x0083C070, output_debug_string, 7);
 }
